1a3b19c20f41712d89f19c1386ca118bb5ad3b91
godara03 <aditya.godara@bbc.co.uk>
2014-03-19 14:26:20 +0000

    price list is an object

--- Checkout.js
+++ Checkout.js
@@ -1,13 +1,20 @@
-function Checkout() {
-    var total = 0;
-
-    this.priceList = {
+function PriceList() {
+    this.prices = {
         "Kiwi": { "price": 50 },
         "Banana": { "price": 75 }
     };
 
+    this.priceFor = function(item) {
+        return this.prices[item].price;
+    }
+}
+
+function Checkout() {
+    var total = 0;
+    this.priceList = new PriceList;
+
     this.scan = function (item) {
-        total += this.priceList[item].price;
+        total += this.priceList.priceFor(item);
     }
 
     this.total = function () {
