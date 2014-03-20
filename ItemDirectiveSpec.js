bfcc6e906228d91a05dc5f4a252259b69fcbf1e3
godara <devcore3d@outlook.com>
2014-03-20 14:01:07 +0000

    test directives

---
+++ ItemDirectiveSpec.js
@@ -0,0 +1,20 @@
+describe("item directive", function() {
+    var $rootScope, element;
+
+    beforeEach(module("CheckoutModule"));
+    beforeEach(inject(function($compile, _$rootScope_) {
+        $rootScope = _$rootScope_;
+        $rootScope.doSomething = jasmine.createSpy();
+        element = angular.element("<item name='Apple' action='doSomething(item)'>test</item>");
+        $compile(element)($rootScope);
+    }));
+
+    it("has a button", function() {
+        expect(element.html()).toContain("button");
+    });
+
+    it("calls the action on click", function() {
+        element.find("button").triggerHandler("click");
+        expect($rootScope.doSomething).toHaveBeenCalledWith('Apple');
+    });
+});
