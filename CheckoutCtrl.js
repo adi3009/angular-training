21e5572e175ccbc1018494fc5ae3b8aebd5a88a3
godara <devcore3d@outlook.com>
2014-03-20 12:36:46 +0000

    price check

--- CheckoutCtrl.js
+++ CheckoutCtrl.js
@@ -1,7 +1,16 @@
-function CheckoutCtrl(Checkout) {
+function CheckoutCtrl(Checkout, PriceList) {
     this.checkout = Checkout
 
     this.add = function(item) {
         this.checkout.scan(item);
+    }
+
+    this.checkedPrice = 0;
+
+    this.priceCheck = function(item) {
+        var that = this;
+        PriceList.priceFor(item).then(function(price) {
+            that.checkedPrice = price;
+        });
     }
 }
--- CheckoutCtrlSpec.js
+++ CheckoutCtrlSpec.js
@@ -1,13 +1,32 @@
 describe("checkout controller", function() {
-    var controller, checkout;
+    var controller, checkout, priceList, d;
 
-    beforeEach(function() {
+    beforeEach(inject(function($q) {
         checkout = jasmine.createSpyObj("checkout", ["scan"]);
-        controller = new CheckoutCtrl(checkout);
-    });
+        priceList = jasmine.createSpyObj("priceList", ["priceFor"]);
+
+        d = $q.defer();
+
+        priceList.priceFor.andReturn(d.promise);
+        controller = new CheckoutCtrl(checkout, priceList);
+    }));
 
     it("increase total on click of add", function() {
         controller.add('Kiwi');
         expect(checkout.scan).toHaveBeenCalledWith("Kiwi");
     });
+
+    it("checks the price and sets checkedPrice", inject(function($rootScope) {
+        d.resolve(25);
+        controller.priceCheck('Kiwi');
+        $rootScope.$apply();
+        expect(controller.checkedPrice).toEqual(25);
+    }));
+
+    it("checks the price and sets checkedPrice", inject(function($rootScope) {
+        d.resolve(42);
+        controller.priceCheck('Kiwi');
+        $rootScope.$apply();
+        expect(controller.checkedPrice).toEqual(42);
+    }));
 });
